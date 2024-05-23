703be38a361e15b7bac350eaab013e511868d01e
Nikolay Chendey <chendey.n@tomhunter.ru>
2024-05-23 09:57:53 +0300

    Fixed Dish component

--- src/components/Dish/Dish.tsx
+++ src/components/Dish/Dish.tsx
@@ -1,7 +1,28 @@
 import { DishData } from "@/constants";
+import { useState } from "react";
+import { Button } from "../Button";
+import { ButtonBlock, Container, Count } from "./styled";
 
 export const Dish = ({ dish }: { dish: DishData }) => {
+  const [count, setCount] = useState(0);
+
   if (!dish) return null;
 
-  return <span>{dish.name}</span>;
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+
+  return (
+    <Container>
+      {dish.name}
+      <ButtonBlock>
+        <Button onClick={decrease} disabled={count === 0}>
+          -
+        </Button>
+        <Count>{count}</Count>
+        <Button onClick={increase} disabled={count === 5}>
+          +
+        </Button>
+      </ButtonBlock>
+    </Container>
+  );
 };
