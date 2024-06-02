8c0f1422a2efbd8d9caf03e87fb7b8bc014ebf84
Nikolay Chendey <chendey.n@tomhunter.ru>
2024-06-02 23:05:32 +0300

    Added user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import { Footer, Header, Restaurants } from "./components";
 import { restaurants } from "./constants";
-import { ThemeContext, ThemeType } from "./contexts";
+import { ThemeContext, ThemeType, UserContext } from "./contexts";
 
 export const App = () => {
   const [theme, setTheme] = useState<ThemeType>("light");
+  const [user, setUser] = useState("");
 
   return (
     <ThemeContext.Provider value={theme}>
-      <Header
-        setTheme={() =>
-          setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
-        }
-      />
-      <Restaurants restaurants={restaurants} />
-      <Footer />
+      <UserContext.Provider value={user}>
+        <Header
+          setTheme={() =>
+            setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+          }
+          setUser={setUser}
+        />
+        <Restaurants restaurants={restaurants} />
+        <Footer />
+      </UserContext.Provider>
     </ThemeContext.Provider>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,31 @@
+import { UserContext } from "@/contexts";
+import { useContext } from "react";
 import styled from "styled-components";
 import { Button } from "../Button";
 
-export const Header = ({ setTheme }: { setTheme: () => void }) => (
-  <Container>
-    <span>Header</span>
-    <Button onClick={setTheme}>Изменить тему</Button>
-  </Container>
-);
+interface Props {
+  setTheme: () => void;
+  setUser: (user: string) => void;
+}
+
+export const Header = ({ setTheme, setUser }: Props) => {
+  const contextUser = useContext(UserContext);
+
+  return (
+    <Container>
+      <span>Header</span>
+      <Button onClick={setTheme}>Изменить тему</Button>
+      {contextUser ? (
+        <div>
+          <span>{contextUser}</span>
+          <Button onClick={() => setUser("")}>Выйти</Button>
+        </div>
+      ) : (
+        <Button onClick={() => setUser("user")}>Войти</Button>
+      )}
+    </Container>
+  );
+};
 
 const Container = styled.div`
   position: fixed;
